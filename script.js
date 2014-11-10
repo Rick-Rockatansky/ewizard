d9a0a6b0ce28c4f2a3438fe290eab9271be97e13
Rick-Rockatansky <akabos3000@gmail.com>
2014-11-10 17:38:58 +0200

    commit

--- script.js
+++ script.js
@@ -1,39 +1,46 @@
 window.onload = function() {
 
-	var a = document.getElementById('intro'),
-		b = document.getElementById('about'),
-		c = document.getElementById('start');
+	// var a = document.getElementById('intro'),
+	// 	b = document.getElementById('about'),
+	// 	c = document.getElementById('contInterface');
 		
-	var d = document.getElementById('catalogs'),
-		e = document.getElementById('actionTypes'),
-		f = document.getElementById('actions'),
-		g = document.getElementById('reports');
+	// var d = document.getElementById('catalogs'),
+	// 	e = document.getElementById('actionTypes'),
+	// 	f = document.getElementById('actions'),
+	// 	g = document.getElementById('reports');
 		
-	a.onclick = function() {
-		a.nextElementSibling.classList.toggle('hide');
-		a.children[0].classList.toggle('down');
-	};
-	b.onclick = function() {
-		b.nextElementSibling.classList.toggle('hide');
-		b.children[0].classList.toggle('down');
-	};
-	c.onclick = function() {
+	// a.onclick = function() {
+	// 	a.nextElementSibling.classList.toggle('hide');
+	// 	a.children[0].classList.toggle('down');
+	// };
+	// b.onclick = function() {
+	// 	b.nextElementSibling.classList.toggle('hide');
+	// 	b.children[0].classList.toggle('down');
+	// };
+	// c.onclick = function() {
+	// 	c.nextElementSibling.classList.toggle('hide');
+	// 	c.children[0].classList.toggle('down');
+	// };
+
+	// d.onclick = function() {
+	// d.nextElementSibling.classList.toggle('hide');
+	// };
+	// e.onclick = function() {
+	// e.nextElementSibling.classList.toggle('hide');
+	// };
+	// f.onclick = function() {
+	// f.nextElementSibling.classList.toggle('hide');
+	// };
+	// g.onclick = function() {
+	// g.nextElementSibling.classList.toggle('hide');
+	// };
+
+
+
+var c = document.getElementById('contInterface');
+
+c.onclick = function() {
 		c.nextElementSibling.classList.toggle('hide');
 		c.children[0].classList.toggle('down');
-	};
-
-	d.onclick = function() {
-	d.nextElementSibling.classList.toggle('hide');
-	};
-	e.onclick = function() {
-	e.nextElementSibling.classList.toggle('hide');
-	};
-	f.onclick = function() {
-	f.nextElementSibling.classList.toggle('hide');
-	};
-	g.onclick = function() {
-	g.nextElementSibling.classList.toggle('hide');
-	};
-
-	
+	};	
 };
