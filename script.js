850548ecbe7a027a2ebda4c3cbcb924a4f79fa46
Rick-Rockatansky <akabos3000@gmail.com>
2014-11-26 10:54:10 +0200

    commit

--- script.js
+++ script.js
@@ -1,44 +1,9 @@
 window.onload = function() {
 
-	// var a = document.getElementById('intro'),
-	// 	b = document.getElementById('about'),
-	// 	c = document.getElementById('contInterface');
-		
-	// var d = document.getElementById('catalogs'),
-	// 	e = document.getElementById('actionTypes'),
-	// 	f = document.getElementById('actions'),
-	// 	g = document.getElementById('reports');
-		
-	// a.onclick = function() {
-	// 	a.nextElementSibling.classList.toggle('hide');
-	// 	a.children[0].classList.toggle('down');
-	// };
-	// b.onclick = function() {
-	// 	b.nextElementSibling.classList.toggle('hide');
-	// 	b.children[0].classList.toggle('down');
-	// };
-	// c.onclick = function() {
-	// 	c.nextElementSibling.classList.toggle('hide');
-	// 	c.children[0].classList.toggle('down');
-	// };
-
-	// d.onclick = function() {
-	// d.nextElementSibling.classList.toggle('hide');
-	// };
-	// e.onclick = function() {
-	// e.nextElementSibling.classList.toggle('hide');
-	// };
-	// f.onclick = function() {
-	// f.nextElementSibling.classList.toggle('hide');
-	// };
-	// g.onclick = function() {
-	// g.nextElementSibling.classList.toggle('hide');
-	// };
-
-
-
 var c = document.getElementById('contInterface');
 var d = document.getElementById('contCreate');
+var e = document.getElementById('contEdit');
+var f = document.getElementById('contVideo');
 
 c.onclick = function() {
 		c.nextElementSibling.classList.toggle('hide');
@@ -48,4 +13,30 @@ c.onclick = function() {
 d.onclick = function() {
 	d.nextElementSibling.classList.toggle('hide');
 	};
+
+e.onclick = function() {
+		e.nextElementSibling.classList.toggle('hide');
+		e.children[0].classList.toggle('down');
+	};
+
+f.onclick = function() {
+		f.nextElementSibling.classList.toggle('hide');
+		f.children[0].classList.toggle('down');
+	};
+
+
+$(window).scroll(function(){
+  if($(window).scrollTop() > 1000){
+      $("#navigation").fadeIn("slow");
+  }
+});
+$(window).scroll(function(){
+  if($(window).scrollTop() < 1000){
+      $("#navigation").fadeOut("fast");
+  }
+});
+
+
+
+
 };
